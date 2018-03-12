1329a900222ce2ddc7c311f72244c6c375434d58
Facundo Zaldo <fzaldo@gmail.com>
2018-03-11 21:24:59 -0300

    Initial works

---
+++ gulpfile.js
@@ -0,0 +1,26 @@
+var gulp = require('gulp');  
+var sass = require('gulp-sass');  
+var browserSync = require('browser-sync');
+
+gulp.task('sass', function () {  
+	gulp.src('assets/scss/app.scss')
+		.pipe(sass({includePaths: ['scss']}))
+		.pipe(gulp.dest('docs/css'))
+	    .pipe(browserSync.stream());
+});
+
+gulp.task('browser-sync', function() {  
+	browserSync.init(["**/*"], {
+		server: {
+			baseDir: "./docs"
+		},
+		browser: '/opt/firefox_dev/firefox',
+	});
+
+	gulp.watch("**/*.scss", ['sass']);
+	gulp.watch("**/*.html").on('change', browserSync.reload);
+});
+
+gulp.task('default', ['sass', 'browser-sync'], function () {  
+
+});
