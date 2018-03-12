efe2e52a0ea6db7f71967056e83f4f1fdac9b8c0
Facundo Zaldo <fzaldo@gmail.com>
2018-03-11 23:32:32 -0300

    Load pages by ajax

--- docs/js/app.js
+++ docs/js/app.js
@@ -1,16 +1,24 @@
 $(document).ready(function () {
-    // toggle sidebar when button clicked
-    $('.sidebar-toggle').on('click', function () {
-        $('.sidebar').toggleClass('toggled');
+	// toggle sidebar when button clicked
+	$('.sidebar-toggle').on('click', function () {
+		$('.sidebar').toggleClass('toggled');
+	});
+
+	// auto-expand submenu if an item is active
+	var active = $('.sidebar .active');
+
+	if (active.length && active.parent('.collapse').length) {
+		var parent = active.parent('.collapse');
+
+		parent.prev('a').attr('aria-expanded', true);
+		parent.addClass('show');
+	}
+
+
+    $(".list a").click(function(){
+		$.get("pages/"+$(this).data('url'), function (data) {
+			$('#body').html(new showdown.Converter().makeHtml(data));
+		});
     });
 
-    // auto-expand submenu if an item is active
-    var active = $('.sidebar .active');
-
-    if (active.length && active.parent('.collapse').length) {
-        var parent = active.parent('.collapse');
-
-        parent.prev('a').attr('aria-expanded', true);
-        parent.addClass('show');
-    }
 });
