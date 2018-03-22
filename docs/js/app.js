64e097d98d6a842311151afee774102fb9a2dc64
Facundo Zaldo <fzaldo@gmail.com>
2018-03-22 01:50:00 -0300

    Code refactorind and some style fixes

--- docs/js/app.js
+++ docs/js/app.js
@@ -3,7 +3,7 @@ var config
 
 $(function(){
 
-	////-- LOAD CONFIG --////
+	////////////// LOAD CONFIG //////////////
 	$.get('./config.yaml', function(file) {
 		config = jsyaml.load(file)
 
@@ -11,8 +11,8 @@ $(function(){
 		$('.logo').html(config.logo)
 		$('#h-title').html(config.title)
 
-		if(config.lang.active) createDrop(config.lang.list, "lang")
-		if(config.versions.active) createDrop(config.versions.list, "version")
+		if(config.lang.active) create.drop(config.lang.list, "lang")
+		if(config.versions.active) create.drop(config.versions.list, "version")
 
 
 	}).fail(function(req, textStatus) {
@@ -20,10 +20,10 @@ $(function(){
 	}).always(function() {
 		$('body').fadeIn(1000)
 
-		////-- LOAD ROUTES --////		
+		////////////// LOAD ROUTES //////////////		
 		$.get('./routes.yaml', function(file) {
 			$.each(jsyaml.load(file), function(i, item) { 
-				$('#list').append(createMenuItem(i, item))
+				$('#list').append(create.menuItem(i, item))
 			})
 
 			if(location.hash.length > 0){
@@ -93,42 +93,92 @@ $(function(){
 });
 
 
-function createDrop(list, value){
-	let elem = $("#drop-"+value);
+var	create = {
+	////////////// DROPS //////////////
+	drop(list, value){
+		let elem = $("#drop-"+value);
 
-	$.each(list, function(i, item) { 
-		let icon = ''
-		
-		if(item.default != undefined){
-			icon = '<i class="fa fa-check"></i>'
-		} 
+		$.each(list, function(i, item) { 
+			let icon = ''
 			
-		elem.append('<li><a href="#">'+item.title+' '+icon+'</a></li>')
-	})
-}
+			if(item.default != undefined){
+				icon = '<i class="fa fa-check"></i>'
+			} 
+				
+			elem.append('<li><a href="#">'+item.title+' '+icon+'</a></li>')
+		})
+	},
 
-function createMenuItem(i, page){
-	if(page.items == undefined){
-		return `<li><a href="#${page.file}" data-url="${page.file}" data-ftitle="${escape(page.title)}" class="menuitem">
-					<span class="b-level1">${page.id}. </span><text>${page.title}</text>
-				</a></li>`
+	////////////// MENUITEM //////////////
+	menuItem(i, page){
+		if(page.items == undefined){
+			return `<li><a href="#${page.file}" data-url="${page.file}" data-ftitle="${escape(page.title)}" class="menuitem">
+						<span class="b-level1">${page.id}. </span><text>${page.title}</text>
+					</a></li>`
+		}
+
+		let html = `<li>
+					 <a href="#${i+1}/${page.folder}" data-url="${page.folder}/chapter.md" data-ftitle="${escape(page.title)}" data-toggle="collapse" class="menuitem">
+						<span class="b-level1">${page.id}. </span><text>${page.title}</text>
+					 </a>
+						<ul id="${i+1}/${page.folder}" class="list-unstyled collapse">`
+
+		$.each(page.items, function(j, item) { 
+			let path = page.folder +'/'+item.file
+			html += `<li><a href="#${path}" data-url="${path}" data-ftitle="${escape(page.title)}" data-title="${escape(item.title)}" class="menuitem">
+						<span class="b-level2">${page.id}.${(j+1)} </span> <text>${item.title}</text>
+					</a></li>`
+		});
+														
+		return html + '</ul></li>'
+	},
+
+
+	////////////// NAVS //////////////
+	navs(active){
+		let prev = $($('.menuitem')[active-1])
+		let next = $($('.menuitem')[active+1])
+
+		let htmlPrev = `<a href="${prev.attr('href')}" class="nav nav-prev col-xs-4" data-url="${prev.data('url')}" data-ftitle="${prev.data('ftitle')}">
+							<i class="fa fa-chevron-left"></i>
+						</a>`
+		
+		let htmlMenu = `<a href="#" id="sidebar-toggle" class="nav col-xs-4" data-sidebar-toggle><i class="fa fa-bars"></i></a>`
+
+
+		let htmlNext = `<a href="${next.attr('href')}" class="nav nav-next col-xs-4" data-url="${next.data('url')}" data-ftitle="${next.data('ftitle')}">
+							<i class="fa fa-chevron-right"></i>
+						</a>`
+
+		if(next.data('url') == undefined || !config.nav.next) htmlNext = ''
+		if(prev.data('url') == undefined || !config.nav.prev) htmlPrev = ''
+
+		$('#navigation').html(htmlPrev+htmlMenu+htmlNext);
+	},
+
+	////////////// BREADCRUMB //////////////
+	breadcrumb(elem){
+		let folder = splitUrl(elem.url)[0]
+		
+		$('#top-bar').remove()
+
+		let html = `<div id="top-bar">
+						<div id="top-bar-buttons">
+							<a class="github-link" href="#"><i class="fa fa-pencil"></i></a>
+						</div>
+						<div id="breadcrumbs" itemscope="" itemtype="http://data-vocabulary.org/Breadcrumb">
+							<a href="#${folder}" itemprop="url" class="bc-url"><span class="bc-folder" itemprop="title">${unescape(elem.ftitle)}</span></a>`
+		
+		if(elem.title !== undefined){
+			html +=	`<i class="fa fa-angle-right bc-separator"></i>
+						<span class="bc-file" itemprop="title">${unescape(elem.title)}</span>`
+		  }
+
+		return html + '</div></div>'
 	}
 
-	let html = `<li>
-				 <a href="#${i+1}/${page.folder}" data-url="${page.folder}/chapter.md" data-ftitle="${escape(page.title)}" data-toggle="collapse" class="menuitem">
-					<span class="b-level1">${page.id}. </span><text>${page.title}</text>
-				 </a>
-					<ul id="${i+1}/${page.folder}" class="list-unstyled collapse">`
-
-	$.each(page.items, function(j, item) { 
-		let path = page.folder +'/'+item.file
-		html += `<li><a href="#${path}" data-url="${path}" data-ftitle="${escape(page.title)}" data-title="${escape(item.title)}" class="menuitem">
-					<span class="b-level2">${page.id}.${(j+1)} </span> <text>${item.title}</text>
-				</a></li>`
-	});
-													
-	return html + '</ul></li>'
 }
+
 
 function changePage(elem){
 	// TODO: Return if la pagina a cambiar es la misma que se clickea
@@ -150,7 +200,7 @@ function changePage(elem){
 			$('#chapter').contents().unwrap()
 		}
 	
-		$('#body-content').prepend(createBreadcrumb(elem.data()))
+		$('#body-content').prepend(create.breadcrumb(elem.data()))
 
 		if(url.endsWith('.md')){	
 			let sd = new showdown.Converter(config.showdown)
@@ -178,7 +228,7 @@ function changePage(elem){
 
 	$.each($('.menuitem'), function(i, menuitem) { 
 		if($(menuitem).hasClass('active')) {
-			createNavs(i)
+			create.navs(i)
 			return false;
 		}
 	})
@@ -188,53 +238,6 @@ function changePage(elem){
 function splitUrl(url){
 	return url.split('/#!/')[0].split('/');
 }
-
-function createNavs(active){
-	let prev = $($('.menuitem')[active-1])
-	let next = $($('.menuitem')[active+1])
-
-	let htmlPrev = `<a href="${prev.attr('href')}" class="nav nav-prev col-xs-4" data-url="${prev.data('url')}" data-ftitle="${prev.data('ftitle')}">
-						<i class="fa fa-chevron-left"></i>
-					</a>`
-	
-	let htmlMenu = `<a href="#" id="sidebar-toggle" class="nav col-xs-4" data-sidebar-toggle><i class="fa fa-bars"></i></a>`
-
-
-	let htmlNext = `<a href="${next.attr('href')}" class="nav nav-next col-xs-4" data-url="${next.data('url')}" data-ftitle="${next.data('ftitle')}">
-						<i class="fa fa-chevron-right"></i>
-					</a>`
-
-
-	if(next.data('url') == undefined || !config.nav.next) htmlNext = ''
-	if(prev.data('url') == undefined || !config.nav.prev) htmlPrev = ''
-
-	$('#navigation').html(htmlPrev+htmlMenu+htmlNext);
-}
-
-
-
-function createBreadcrumb(elem){
-	let folder = splitUrl(elem.url)[0]
-	
-	$('#top-bar').remove()
-
-	let html = `<div id="top-bar">
-					<div id="top-bar-buttons">
-						<a class="github-link" href="#"><i class="fa fa-pencil"></i></a>
-					</div>
-					<div id="breadcrumbs" itemscope="" itemtype="http://data-vocabulary.org/Breadcrumb">
-						<a href="#${folder}" itemprop="url" class="bc-url"><span class="bc-folder" itemprop="title">${unescape(elem.ftitle)}</span></a>`
-	
-	if(elem.title !== undefined){
-		html +=	`<i class="fa fa-angle-right bc-separator"></i>
-					<span class="bc-file" itemprop="title">${unescape(elem.title)}</span>`
-	  }
-
-	return html + '</div></div>'
-}
-
-
-
 
 /// ADENTRO DE READY
 /*
