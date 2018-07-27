e56e12e5aabd8ed57a23cabd84ae35fc6d684a8f
Facundo Zaldo <fzaldo@gmail.com>
2018-07-26 23:22:58 -0300

    Indentation fix

--- gulpfile.js
+++ gulpfile.js
@@ -1,34 +1,34 @@
- "use strict"
+"use strict"
 
-var gulp  = require('gulp'),
-	sass  = require('gulp-sass'),
-	bs    = require('browser-sync'),
-	fs    = require('fs-extra'),
-	zip   = require('gulp-zip'),
-	argv  = require('yargs').argv,
+var gulp 	= require('gulp'),
+	sass 	= require('gulp-sass'),
+	bs 		= require('browser-sync'),
+	fs 		= require('fs-extra'),
+	zip 	= require('gulp-zip'),
+	argv 	= require('yargs').argv,
 	storage = require('gulp-storage')(gulp);
 
 gulp.storage.create('root', 'config.json')
 
 
-gulp.task('default', ['temp','watch'], function () {
-	console.log(' - INFO:\n\tTHEME: '+gulp.storage.get('theme')+'\n\tDATA: '+gulp.storage.get('data')+'\n')
+gulp.task('default', ['temp', 'watch'], function () {
+	console.log(' - INFO:\n\tTHEME: ' + gulp.storage.get('theme') + '\n\tDATA: ' + gulp.storage.get('data') + '\n')
 })
 
-gulp.task('sass', function () {  
+gulp.task('sass', function () {
 	let theme = (argv.theme != undefined) ? argv.theme : gulp.storage.get('theme')
-	console.log("SASS: "+theme)
-	gulp.src('./src/themes/'+theme+'/scss/style.scss')
-		.pipe(sass({includePaths: ['scss']}))
-		.pipe(gulp.dest('./src/themes/'+theme+'/css'))
+	console.log("SASS: " + theme)
+	gulp.src('./src/themes/' + theme + '/scss/style.scss')
+		.pipe(sass({ includePaths: ['scss'] }))
+		.pipe(gulp.dest('./src/themes/' + theme + '/css'))
 		.pipe(bs.stream());
 
 })
 
 // $ gulp watch --theme 'default' --data 'basic'
-gulp.task('watch', function() {  
+gulp.task('watch', function () {
 	let theme = (argv.theme == undefined) ? gulp.storage.get('theme') : argv.theme
-	let folder = (argv.data == undefined) ? gulp.storage.get('data')  : argv.data
+	let folder = (argv.data == undefined) ? gulp.storage.get('data') : argv.data
 
 	bs.init(["./src/**/*"], {
 		server: {
@@ -37,7 +37,7 @@ gulp.task('watch', function() {
 		browser: '/opt/firefox_dev/firefox',
 	})
 
-	gulp.watch("./src/**/*.scss", ['sass']);	
+	gulp.watch("./src/**/*.scss", ['sass']);
 	gulp.watch("./src/**/*", ['temp']);
 	//gulp.watch("**/*.html").on('change', bs.reload);
 
@@ -47,29 +47,29 @@ gulp.task('watch', function() {
 //  - Zip temp folder and save on ./exported
 // $ gulp export --theme 'default'
 //  - Zip theme and save on ./exported
-gulp.task('export', function(){ 
+gulp.task('export', function () {
 	let now = new Date()
 
-	let filename = argv.file || ''+now.getFullYear()+now.getMonth()+1+now.getDate()+now.getMinutes()+now.getSeconds()
-	let src = argv.theme ? './src/themes/'+argv.theme+'/**/*' : './temp/**/*'
-	
+	let filename = argv.file || '' + now.getFullYear() + now.getMonth() + 1 + now.getDate() + now.getMinutes() + now.getSeconds()
+	let src = argv.theme ? './src/themes/' + argv.theme + '/**/*' : './temp/**/*'
+
 	gulp.src(src)
 		.pipe(zip(filename))
 		.pipe(gulp.dest('./exported'))
 
-	console.log(' - INFO:\n\tTHEME: '+gulp.storage.get('theme')+'\n\tDATA: '+gulp.storage.get('data')+'\n')
-	console.log('   File created ./exported/'+filename+'.zip')
+	console.log(' - INFO:\n\tTHEME: ' + gulp.storage.get('theme') + '\n\tDATA: ' + gulp.storage.get('data') + '\n')
+	console.log('   File created ./exported/' + filename + '.zip')
 })
 
 
-gulp.task('clean', function() {
+gulp.task('clean', function () {
 	fs.removeSync('./temp')
 	console.log(" - INFO: ./temp has been remove")
 })
 
 // $ gulp active --data 'basic' --theme 'default'
-gulp.task('active', function(){ 
-	if(argv.data != undefined) {
+gulp.task('active', function () {
+	if (argv.data != undefined) {
 		store(argv.theme || 'default', argv.data)
 
 		gulp.start('temp')
@@ -79,60 +79,60 @@ gulp.task('active', function(){
 })
 
 
-gulp.task('temp', function() {
+gulp.task('temp', function () {
 
 	// FIX: theme copy
 	let theme = gulp.storage.get('theme')
 	let folder = gulp.storage.get('data')
-	
+
 	try {
-		fs.accessSync('./src/data/'+folder+'/')
+		fs.accessSync('./src/data/' + folder + '/')
 
 		gulp.start('clean')
 
 		gulp.src(['./src/app/**/*']).pipe(gulp.dest('./temp'))
 		console.log(" - INFO: ./src/app/**/* has been copied to ./temp")
 
-		gulp.src('./src/data/'+folder+'/**/*').pipe(gulp.dest('./temp'))
-		console.log(" - INFO: ./src/data/"+folder+"/* has been copied to ./temp")
+		gulp.src('./src/data/' + folder + '/**/*').pipe(gulp.dest('./temp'))
+		console.log(" - INFO: ./src/data/" + folder + "/* has been copied to ./temp")
 
-		gulp.src('./src/themes/'+theme+'/**/*', { "base" : './src/themes/' }).pipe(gulp.dest('temp/themes'))
-		console.log(" - INFO: ./src/themes/"+theme+"/**/* has been copied to ./temp/themes")
+		gulp.src('./src/themes/' + theme + '/**/*', { "base": './src/themes/' }).pipe(gulp.dest('temp/themes'))
+		console.log(" - INFO: ./src/themes/" + theme + "/**/* has been copied to ./temp/themes")
 
-	} catch(e) {
-		console.log(" - ERROR: './src/data/"+folder+"' doesn't exist!")
+	} catch (e) {
+		console.log(" - ERROR: './src/data/" + folder + "' doesn't exist!")
 
 	}
 })
 
 
 // $ gulp store --theme 'default' --data 'basic'
-gulp.task('store', function() {
+gulp.task('store', function () {
 	let theme = (argv.theme == undefined) ? 'default' : argv.theme
-	let data  = (argv.data == undefined)  ? 'basic'   : argv.data
+	let data = (argv.data == undefined) ? 'basic' : argv.data
 	store(theme, data)
 });
 
-function store(theme, data){
+function store(theme, data) {
 	try {
-		fs.accessSync('./src/themes/'+theme)
-		fs.accessSync('./src/data/'+data)
+		fs.accessSync('./src/themes/' + theme)
+		fs.accessSync('./src/data/' + data)
 
 		gulp.storage.set('theme', theme)
 		gulp.storage.set('data', data)
 
-		console.log(" - INFO: config.json file has been created with '"+gulp.storage.get('theme')+"' and '"+gulp.storage.get('data')+"' vars")
-	
-	}catch(e){
+		console.log(" - INFO: config.json file has been created with '" + gulp.storage.get('theme') + "' and '" + gulp.storage.get('data') + "' vars")
+
+	} catch (e) {
 		console.log(" - ERROR: The folder does not exist.")
 	}
 }
 
 
 // Shortcuts
-gulp.task('gitowl',   function(){ folder = 'gitowl';   gulp.start('temp') })
-gulp.task('basic',    function(){ folder = 'basic';    gulp.start('temp') })
-gulp.task('advanced', function(){ folder = 'advanced'; gulp.start('temp') })
+gulp.task('gitowl',   function () { folder = 'gitowl'; gulp.start('temp') })
+gulp.task('basic',    function () { folder = 'basic'; gulp.start('temp') })
+gulp.task('advanced', function () { folder = 'advanced'; gulp.start('temp') })
 
 
 
--- src/app/gitowl.js
+++ src/app/gitowl.js
@@ -1,20 +1,20 @@
- "use strict"
- var config, 
- active = {lang:null, version:null, path:'/'}
+"use strict"
 
- $(function(){
+var config, active = { lang: null, version: null, path: '/' }
+
+$(function () {
 	////////////// LOAD CONFIG //////////////
-	$.get('./config.yaml', function(file) {
+	$.get('./config.yaml', function (file) {
 		config = jsyaml.load(file)
-		$('head').append('<link rel="stylesheet" href="themes/'+config.theme+'/css/style.css"/>')
-		
+		$('head').append('<link rel="stylesheet" href="themes/' + config.theme + '/css/style.css"/>')
+
 		NProgress.configure(config.nprogress)
 		NProgress.start()
-		
+
 		$('.logo').html(config.content.logo)
 		$('#h-title').html(config.content.title)
 		$('.searchbox input').attr("placeholder", config.search.text);
-		if(!config.search.active) $('.searchbox').hide()
+		if (!config.search.active) $('.searchbox').hide()
 
 		change.paths(config.paths)
 
@@ -22,174 +22,176 @@
 		create.drop('version')
 		NProgress.set(0.4);
 
-	}).fail(function(req, status) { utils.show.sidebarError('ERROR: config.yaml') 
-	}).always(function() {
+	}).fail(function (req, status) {
+		utils.show.sidebarError('ERROR: config.yaml')
+	}).always(function () {
 		$('body').fadeIn(1000)
 
 		////////////// LOAD ROUTES //////////////		
-		$.get(active.path+'routes.yaml', function(file) {
+		$.get(active.path + 'routes.yaml', function (file) {
 			NProgress.set(0.8);
-			$.each(jsyaml.load(file), function(i, item) { 
+			$.each(jsyaml.load(file), function (i, item) {
 				$('#list').append(create.menuItem(i, item))
 			})
-			
-			if(location.hash.length > 0){
-				if(config.lang.active){
-					let elemVersion = $('[data-type="lang"][data-path="'+utils.splitUrl(location.hash)[1]+'"]')
-					if(!elemVersion.find('i').length) change.active(elemVersion)
+
+			if (location.hash.length > 0) {
+				if (config.lang.active) {
+					let elemVersion = $('[data-type="lang"][data-path="' + utils.splitUrl(location.hash)[1] + '"]')
+					if (!elemVersion.find('i').length) change.active(elemVersion)
 				}
 
-				if(config.version.active && !config.lang.active){
-					let elemLang = $('[data-type="lang"][data-path="'+utils.splitUrl(location.hash)[2]+'"]')
-					if(!elemLang.find('i').length) change.active(elemLang)
+				if (config.version.active && !config.lang.active) {
+					let elemLang = $('[data-type="lang"][data-path="' + utils.splitUrl(location.hash)[2] + '"]')
+					if (!elemLang.find('i').length) change.active(elemLang)
 				}
 
-				change.page($("#list li a[href='"+location.hash+"']"))
-				$('.active').closest('ul').collapse('show') 
+				change.page($("#list li a[href='" + location.hash + "']"))
+				$('.active').closest('ul').collapse('show')
 			} else {
 				change.home()
 			}
 
-		}).fail(function(req, status) { utils.show.sidebarError('ERROR: routes.yaml')
-	}).always(function() {
-		NProgress.done()
-		new PerfectScrollbar('#scroll', config.perfectscrollbar);
-		$('#loading-sidebar').hide()
-	})
-})
-
-$('#search-by').on('keyup', function() {
-	let search = $(this).val()
-
-	$('.sb-close').show()
-
-	$('#list li').filter(function() {
-		$(this).toggle($(this).text().toLowerCase().indexOf(search.toLowerCase()) > -1)
+		}).fail(function (req, status) {
+			utils.show.sidebarError('ERROR: routes.yaml')
+		}).always(function () {
+			NProgress.done()
+			new PerfectScrollbar('#scroll', config.perfectscrollbar);
+			$('#loading-sidebar').hide()
+		})
 	})
 
-	if($(this).val().length == 0){
-		$('#list li ul').not($('.active').parent().parent()).collapse('hide')
-	}else{
-		$('#list li ul').collapse('show')
-	}
+	$('#search-by').on('keyup', function () {
+		let search = $(this).val()
 
-	$('text').each(function(i, item) {
-		let re = new RegExp(search, 'i')
-		$(item).html($(item).text().replace(re, '<high>'+re.exec($(item).text())+'</high>'))
+		$('.sb-close').show()
+
+		$('#list li').filter(function () {
+			$(this).toggle($(this).text().toLowerCase().indexOf(search.toLowerCase()) > -1)
+		})
+
+		if ($(this).val().length == 0) {
+			$('#list li ul').not($('.active').parent().parent()).collapse('hide')
+		} else {
+			$('#list li ul').collapse('show')
+		}
+
+		$('text').each(function (i, item) {
+			let re = new RegExp(search, 'i')
+			$(item).html($(item).text().replace(re, '<high>' + re.exec($(item).text()) + '</high>'))
+		})
 	})
-})
 
-$('.searchbox').on('click','.sb-close',  function() {
-	$('#search-by').val('').keyup()
-	$('.sb-close').hide()
-})
+	$('.searchbox').on('click', '.sb-close', function () {
+		$('#search-by').val('').keyup()
+		$('.sb-close').hide()
+	})
 
 
-$('#list').on('click', 'li a', function() {
-	if(!$(this).hasClass('active')) change.page($(this))
-	$('#sidebar').removeClass('toggled')
-})
+	$('#list').on('click', 'li a', function () {
+		if (!$(this).hasClass('active')) change.page($(this))
+		$('#sidebar').removeClass('toggled')
+	})
 
-$('.dropdown').on('click', 'li a', function() {
-	if(!$(this).find('i').length) {
-		change.active($(this))
-		change.home()
-	}
-	$('#sidebar').removeClass('toggled')
-})
+	$('.dropdown').on('click', 'li a', function () {
+		if (!$(this).find('i').length) {
+			change.active($(this))
+			change.home()
+		}
+		$('#sidebar').removeClass('toggled')
+	})
 
-$('#navigation').on('click', 'a.nav-prev, a.nav-next', function() {
-	let elem = $($("a[href='"+$(this).attr('href')+"']")).show()
-	elem.parent().parent().collapse('show')
-	change.page(elem)
+	$('#navigation').on('click', 'a.nav-prev, a.nav-next', function () {
+		let elem = $($("a[href='" + $(this).attr('href') + "']")).show()
+		elem.parent().parent().collapse('show')
+		change.page(elem)
 
-}).on('click','a.nav-menu', function () {
-	$('#sidebar').addClass('toggled')
-})
+	}).on('click', 'a.nav-menu', function () {
+		$('#sidebar').addClass('toggled')
+	})
 
-$('#footer').on('click', function() {
-	$(this).find('a')[0].click()
-})
+	$('#footer').on('click', function () {
+		$(this).find('a')[0].click()
+	})
 
-$("body").keypress(function(e){
-		if(!config.nav.keypress || $(e.target).is('input,select,button')) return false
+	$("body").keypress(function (e) {
 
-		switch(e.key){
-			case 'j': 
+		if (!config.nav.keypress || $(e.target).is('input,select,button')) return false
+
+		switch (e.key) {
+			case 'j':
 			case 'ArrowLeft':
 				$('a.nav-prev').click()
 				break;
 
-			case 'k': 
-			case 'ArrowRight': 
+			case 'k':
+			case 'ArrowRight':
 				$('a.nav-next').click()
 				break;
 			default:
 				return; // exit this handler for other keys
-    	}
-    	e.preventDefault();
+		}
+		e.preventDefault();
+	})
 
-	});
-
-});
+})
 
 
- var create = {
+var create = {
 
 	////////////// DROPS //////////////
-	drop(type){
-		if(!config[type].active) return $('#menu-'+type).parent('.dropdown').hide()
-			if(config[type].hide) $('#menu-'+type).parent('.dropdown').hide()
+	drop(type) {
+		if (!config[type].active) return $('#menu-' + type).parent('.dropdown').hide()
+		if (config[type].hide) $('#menu-' + type).parent('.dropdown').hide()
 
-				let elem = $("#drop-"+type)
-			let paths
+		let elem = $("#drop-" + type)
+		let paths
 
-			if(type == 'version'){
-				paths = config.lang.active ? active.lang.versions : config.paths
-			} else {
-				paths = config.paths
+		if (type == 'version') {
+			paths = config.lang.active ? active.lang.versions : config.paths
+		} else {
+			paths = config.paths
+		}
+
+		$.each(paths, function (i, item) {
+			let icon = ''
+			if (item.default != undefined) {
+				icon = '<i id="check-' + type + '" class="fa fa-check"></i>'
+				elem.parent().find('text').text(item.title)
 			}
 
-			$.each(paths, function(i, item) { 
-				let icon = ''
-				if(item.default != undefined){
-					icon = '<i id="check-'+type+'" class="fa fa-check"></i>'
-					elem.parent().find('text').text(item.title)
-				} 
-				
-				elem.append('<li><a href="#" data-path="'+item.path+'" data-type="'+type+'">'+item.title+' '+icon+'</a></li>')
-			})
-		},
+			elem.append('<li><a href="#" data-path="' + item.path + '" data-type="' + type + '">' + item.title + ' ' + icon + '</a></li>')
+		})
+	},
 
 
 	////////////// MENUITEM //////////////
-	menuItem(i, page){
+	menuItem(i, page) {
 		let html
 
-		if(page.items == undefined){
+		if (page.items == undefined) {
 			let href = active.path + utils.removeExt(page.file),
-			url  = active.path + config.pages.folder + '/' + page.file;
+				url = active.path + config.pages.folder + '/' + page.file;
 
 			html = `<li><a href="#${href}" data-url="${url}" data-ftitle="${escape(page.title)}" class="menuitem">`
 			html += config.bullet.level1 ? `<span class="b-level1">${page.id}. </span>` : ''
 			return html + `<text>${page.title}</text></a></li>`
 		}
 
-		let	href2  = active.path + page.folder,
-		folder = active.path + config.pages.folder + '/' + page.folder;
+		let href2 = active.path + page.folder,
+			folder = active.path + config.pages.folder + '/' + page.folder;
 
-		html = `<li><a href="#${href2}" data-target="#${i+1}-${page.folder}" data-url="${folder}/${config.pages.chapter}" data-ftitle="${escape(page.title)}" data-toggle="collapse" class="menuitem">`
+		html = `<li><a href="#${href2}" data-target="#${i + 1}-${page.folder}" data-url="${folder}/${config.pages.chapter}" data-ftitle="${escape(page.title)}" data-toggle="collapse" class="menuitem">`
 		html += config.bullet.level1 ? `<span class="b-level1">${page.id}. </span>` : ''
 		html += `<text>${page.title}</text>`
-		html += `</a><ul id="${i+1}-${page.folder}" class="list-unstyled collapse">`
+		html += `</a><ul id="${i + 1}-${page.folder}" class="list-unstyled collapse">`
 
-		$.each(page.items, function(j, item) { 
-			let path  = active.path + config.pages.folder + '/' + page.folder + '/' + item.file,
-			href3 = active.path + page.folder + '/' + utils.removeExt(item.file);
+		$.each(page.items, function (j, item) {
+			let path = active.path + config.pages.folder + '/' + page.folder + '/' + item.file,
+				href3 = active.path + page.folder + '/' + utils.removeExt(item.file);
 
 			html += `<li><a href="#${href3}" data-url="${path}" data-ftitle="${escape(page.title)}" data-title="${escape(item.title)}" class="menuitem">`
-			html += config.bullet.level2 ? `<span class="b-level2">${page.id}.${(j+1)} </span> ` : ''
-			html +=	`<text>${item.title}</text></a></li>`
+			html += config.bullet.level2 ? `<span class="b-level2">${page.id}.${(j + 1)} </span> ` : ''
+			html += `<text>${item.title}</text></a></li>`
 		});
 
 		return html + '</ul></li>'
@@ -197,43 +199,43 @@ $("body").keypress(function(e){
 
 
 	////////////// NAVS //////////////
-	navs(active){
-		let prev = $($('.menuitem')[active-1])
-		let next = $($('.menuitem')[active+1])
+	navs(active) {
+		let prev = $($('.menuitem')[active - 1])
+		let next = $($('.menuitem')[active + 1])
 
 		let htmlPrev = `<a href="${prev.attr('href')}" class="nav nav-prev col-xs-4" data-url="${prev.data('url')}" data-ftitle="${prev.data('ftitle')}">
-		<i class="fa fa-chevron-left"></i>
-		</a>`
-		
+						<i class="fa fa-chevron-left"></i>
+						</a>`
+
 		let htmlMenu = `<a href="#" id="sidebar-toggle" class="nav nav-menu col-xs-4" data-sidebar-toggle><i class="fa fa-bars"></i></a>`
 
 
 		let htmlNext = `<a href="${next.attr('href')}" class="nav nav-next col-xs-4" data-url="${next.data('url')}" data-ftitle="${next.data('ftitle')}">
-		<i class="fa fa-chevron-right"></i>
-		</a>`
+						<i class="fa fa-chevron-right"></i>
+						</a>`
 
-		if(next.data('url') == undefined || !config.nav.next) htmlNext = ''
-			if(prev.data('url') == undefined || !config.nav.prev) htmlPrev = ''
+		if (next.data('url') == undefined || !config.nav.next) htmlNext = ''
+		if (prev.data('url') == undefined || !config.nav.prev) htmlPrev = ''
 
-				$('#navigation').html(htmlPrev+htmlMenu+htmlNext);
-		},
+		$('#navigation').html(htmlPrev + htmlMenu + htmlNext)
+	},
 
 
 	////////////// BREADCRUMB //////////////
-	breadcrumb(elem){
+	breadcrumb(elem) {
 		let folder = utils.splitUrl(elem.url)[0]
-		
+
 		$('#top-bar').remove()
 
 		let html = `<div id="top-bar">
-		<div id="top-bar-buttons">
-		<a class="github-link" href="#"><i class="fa fa-pencil"></i></a>
-		</div>
-		<div id="breadcrumbs" itemscope="" itemtype="http://data-vocabulary.org/Breadcrumb">
-		<span class="bc-folder" itemprop="title">${unescape(elem.ftitle)}</span>`
+					<div id="top-bar-buttons">
+					<a class="github-link" href="#"><i class="fa fa-pencil"></i></a>
+					</div>
+					<div id="breadcrumbs" itemscope="" itemtype="http://data-vocabulary.org/Breadcrumb">
+					<span class="bc-folder" itemprop="title">${unescape(elem.ftitle)}</span>`
 
-		if(elem.title !== undefined){
-			html +=	`<i class="fa fa-angle-right bc-separator"></i>
+		if (elem.title !== undefined) {
+			html += `<i class="fa fa-angle-right bc-separator"></i>
 			<span class="bc-file" itemprop="title">${unescape(elem.title)}</span>`
 		}
 
@@ -243,93 +245,96 @@ $("body").keypress(function(e){
 }
 
 
-var	change = {
+var change = {
 
-	home(){
+	home() {
 		location.hash = ''
 		$('#top-bar').remove()
 
-		if(!config.pages.home) return $('.menuitem').first().click()
+		if (!config.pages.home) return $('.menuitem').first().click()
 
-			let url = active.path + config.pages.folder + '/' + config.pages.home 
+		let url = active.path + config.pages.folder + '/' + config.pages.home
 		$('#body-inner').html('')
 
-		$.get(url, function(data) {
+		$.get(url, function (data) {
 			$('#error-body').hide()
 
-			if(!$('#chapter').length){
+			if (!$('#chapter').length) {
 				$('#body-inner').wrap('<div id="chapter"></div>')
 			}
-			
+
 			$('#body-inner').html(url.endsWith('.md') ? new showdown.Converter(config.showdown).makeHtml(data) : data)
-			
-		}).fail(function(req, textStatus) {	utils.show.bodyError(req) })
+
+		}).fail(function (req, textStatus) { utils.show.bodyError(req) })
 
 		create.navs(-1)
 
 	},
 
-	page(elem){
-		if(!elem.data('url')) return utils.show.bodyError({status:'404'})
+	page(elem) {
+		if (!elem.data('url')) return utils.show.bodyError({ status: '404' })
 
 		let url = elem.data('url'),
-		isChapter = url.endsWith('/'+config.pages.chapter);
+			isChapter = url.endsWith('/' + config.pages.chapter)
 
-		if(isChapter && !config.pages.chapter) return elem.next('ul').find('.menuitem')[0].click()
+		if (isChapter && !config.pages.chapter) return elem.next('ul').find('.menuitem')[0].click()
 
 		NProgress.start()
 
-		$.get(url, function(data) {
+		$.get(url, function (data) {
 			NProgress.set(0.4)
 			$('#error-body').hide()
 
-			if(isChapter){				
-				if(!$('#chapter').length){
+			if (isChapter) {
+				if (!$('#chapter').length) {
 					$('#body-inner').wrap('<div id="chapter"></div>')
 				}
 				location.hash = elem.prop('hash')
-				
+
 			} else {
 				$('#chapter').contents().unwrap()
 			}
-			
-			if(config.breadcrumb.active) $('#body-content').prepend(create.breadcrumb(elem.data()))
 
-				$('#body-inner').html(url.endsWith('.md') ? new showdown.Converter(config.showdown).makeHtml(data) : data)
-	
-			$('pre code').each(function(i, block) {
+			if (config.breadcrumb.active) $('#body-content').prepend(create.breadcrumb(elem.data()))
+
+			$('#body-inner').html(url.endsWith('.md') ? new showdown.Converter(config.showdown).makeHtml(data) : data)
+
+			$('pre code').each(function (i, block) {
 				hljs.highlightBlock(block);
 			})
-			
-		}).fail(function(req, textStatus) { utils.show.bodyError(req) 
-		}).always(function() { NProgress.done() })
+
+		}).fail(function (req, textStatus) {
+			utils.show.bodyError(req)
+		}).always(function () {
+			NProgress.done()
+		})
 
 
-		$('#list li a').removeClass('active')	
+		$('#list li a').removeClass('active')
 		elem.addClass('active').blur()
 
 		$('#list li ul').not(elem.parent().parent()).collapse('hide')
-		
 
-		$.each($('.menuitem'), function(i, menuitem) { 
-			if($(menuitem).hasClass('active')) return create.navs(i)
+
+		$.each($('.menuitem'), function (i, menuitem) {
+			if ($(menuitem).hasClass('active')) return create.navs(i)
 		})
 
 	},
 
-	active(elem){
+	active(elem) {
 		let type = elem.data('type'),
 			path = elem.data('path'),
 			text = elem.text(),
 			paths;
 
-		$('#check-'+type).remove()
-		$('#menu-'+type+' text').text(text)
+		$('#check-' + type).remove()
+		$('#menu-' + type + ' text').text(text)
 
-		elem.append('<i id="check-'+type+'" class="fa fa-check"></i>')
+		elem.append('<i id="check-' + type + '" class="fa fa-check"></i>')
 
-		if(config.lang.active && config.version.active){
-			if(type == 'lang'){
+		if (config.lang.active && config.version.active) {
+			if (type == 'lang') {
 				paths = [{
 					path: path,
 					title: text,
@@ -342,17 +347,17 @@ var	change = {
 				}]
 			}
 
-			if(type == 'version'){
+			if (type == 'version') {
 				paths = [{
 					path: active.lang.path,
 					title: active.lang.title,
 					default: true,
-					versions: [{ path: path, title: text, default: true	}]
+					versions: [{ path: path, title: text, default: true }]
 				}]
 			}
 
 		} else {
-			if(config.lang.active || config.version.active){
+			if (config.lang.active || config.version.active) {
 				paths = [{
 					path: path,
 					title: text,
@@ -369,10 +374,10 @@ var	change = {
 	},
 
 
-	paths(paths){
-		if(config.lang.active){
-			$.each(paths, function(i, lang) { 
-				if(lang.default){
+	paths(paths) {
+		if (config.lang.active) {
+			$.each(paths, function (i, lang) {
+				if (lang.default) {
 					active.lang = lang
 					active.path += lang.path + '/'
 					return false
@@ -380,67 +385,68 @@ var	change = {
 			})
 		}
 
-		if(config.version.active){
+		if (config.version.active) {
 			let items = active.lang ? active.lang.versions : paths
 
-			$.each(items, function(i, version) { 
-				if(version.default){
+			$.each(items, function (i, version) {
+				if (version.default) {
 					active.version = version
 					active.path += version.path + '/'
 					return false
 				}
 			})
-		}		
+		}
 	},
 
-	menuitems(){
+	menuitems() {
 		$('#list').html('')
 		$('#loading-sidebar').show()
 
-		$.get(active.path+'routes.yaml', function(file) {
-			$.each(jsyaml.load(file), function(i, item) { 
+		$.get(active.path + 'routes.yaml', function (file) {
+			$.each(jsyaml.load(file), function (i, item) {
 				$('#list').append(create.menuItem(i, item))
 			})
 
-			if(location.hash.length > 0){
-				let elem = $("#list li a[href='"+location.hash+"']")
-				
+			if (location.hash.length > 0) {
+				let elem = $("#list li a[href='" + location.hash + "']")
+
 				change.page(elem)
-				$('.active').closest('ul').collapse('show') 
-			} 
+				$('.active').closest('ul').collapse('show')
+			}
 
-		}).fail(function(req, status) { utils.show.sidebarError('ERROR: routes.yaml (Menuitems)')
-	}).always(function() {
-		$('#loading-sidebar').hide()
-	})
+		}).fail(function (req, status) {
+			utils.show.sidebarError('ERROR: routes.yaml (Menuitems)')
+		}).always(function () {
+			$('#loading-sidebar').hide()
+		})
 
-}
+	}
 
 }
 
 var utils = {
 
-	splitUrl(url){
+	splitUrl(url) {
 		return url.split('/#!/')[0].split('/')
 	},
 
-	removeExt(file){
+	removeExt(file) {
 		return file.replace(new RegExp(/(\.md$|\.html?$)/i), '')
 	},
 
 	show: {
-		sidebarError(msj){
+		sidebarError(msj) {
 			$('#error-sidebar').show().css('display', 'block')
 			//$('.dropdown').hide()
 			console.log(msj)
 			NProgress.done()
 		},
 
-		bodyError(msj){
+		bodyError(msj) {
 			$('#body-inner').html('')
-			// $('#error-body').show().find('p').text('Error '+msj.status)
+			//$('#error-body').show().find('p').text('Error ' + msj.status);
 			$('#error-body').show()
-			$('#error-body p').text('Error '+msj.status)
+			$('#error-body p').text('Error ' + msj.status)
 			console.log(msj)
 			NProgress.done()
 		}
